943b5701bb68adf11138d35a8851a82c63b6c4e6
chelsea <cchelsea.wang@mail.utoronto.ca>
2023-11-07 19:40:29 -0500

    started accounts model

--- P2/PetPal/accounts/models.py
+++ P2/PetPal/accounts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.contenttypes.fields import GenericRelation
 
 # Create your models here.
+class Seeker(models.Model):
+    
+    notifications = GenericRelation('Notification')
+
+
+class Shelter(models.Model):
+
+    notifications = GenericRelation('Notification')
