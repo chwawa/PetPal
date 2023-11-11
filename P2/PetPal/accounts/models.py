03bea3bb486e8e1f2db8a4453f08cc558d7e13d1
nomiko0626 <113321610+nomiko0626@users.noreply.github.com>
2023-11-11 15:56:46 -0500

    initial accounts app

--- P2/PetPal/accounts/models.py
+++ P2/PetPal/accounts/models.py
@@ -1,12 +1,32 @@
 from django.db import models
-from django.contrib.contenttypes.fields import GenericRelation
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
-class Seeker(models.Model):
-    
-    notifications = GenericRelation('Notification')
+class CustomUser(AbstractUser):
+    name = models.CharField(max_length=255)
+    profile_pic = models.ImageField(upload_to='profile_pics/', null=True, blank=True)
+    email = models.EmailField(unique=False)
+    phone = models.CharField(max_length=15, null=True, blank=True)
+    location = models.CharField(max_length=255, null=True, blank=True)
+    about = models.TextField(null=True, blank=True)
+    USER_TYPE_CHOICES = [
+        ('seeker', 'Seeker'),
+        ('shelter', 'Shelter'),
+    ]
+    user_type = models.CharField(max_length=10, choices=USER_TYPE_CHOICES)
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name='customuser_set',
+        blank=True,
+        verbose_name='groups',
+        help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.',
+    )
 
-
-class Shelter(models.Model):
-
-    notifications = GenericRelation('Notification')
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='customuser_set',
+        blank=True,
+        verbose_name='user permissions',
+        help_text='Specific permissions for this user.',
+    )
+    def __str__(self) -> str:
+        return str(self.id) + ' ' + self.name
