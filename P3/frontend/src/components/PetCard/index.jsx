306e5c2b35e8ea6538da7dc7569ab4b73cd457f9
Chelsea Wang <chelseawang9@gmail.com>
2023-12-04 00:07:42 -0500

    added data to db; minor changes to pet filtering in db

--- P3/frontend/src/components/PetCard/index.jsx
+++ P3/frontend/src/components/PetCard/index.jsx
@@ -5,7 +5,7 @@ import "./PetCard.css"
 
 function PetCard( { cardImage, cardTitle, cardText } ) {
   return (
-    <Card style={{ cursor: "pointer" }}>
+    <Card style={{ cursor: "pointer"}}>
       <Card.Img className="card-img-top" variant="top" src={cardImage} />
       <Card.Body>
         <Card.Title>{cardTitle}</Card.Title>
