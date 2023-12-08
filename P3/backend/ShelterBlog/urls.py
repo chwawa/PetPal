756f2483d861e0e262edf646b059bbe75f15790f
Elyse Ando <elysekando@gmail.com>
2023-12-08 17:30:44 -0500

    Done views, permissions, serializer, and urls.

--- P3/backend/ShelterBlog/urls.py
+++ P3/backend/ShelterBlog/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from .views import ShelterBlogCreateView, ShelterBlogRetrieveUpdateDestroyView, ShelterBlogListView
 
-app_name='shelterblog'
+app_name='shelter'
 urlpatterns = [
-    path('', ShelterBlogListView.as_view(), name='shelter-blog-list'),
-    path('create/', ShelterBlogCreateView.as_view(), name='shelter-blog-create'),
-    path('<int:pk>/', ShelterBlogRetrieveUpdateDestroyView.as_view(), name='shelter-blog-detail'),
+    path('<int:shelter_id>/blog/', ShelterBlogListView.as_view(), name='shelter-blog-list'),
+    path('<int:shelter_id>/blog/create/', ShelterBlogCreateView.as_view(), name='shelter-blog-create'),
+    path('<int:shelter_id>/blog/<int:shelterblog_id>/', ShelterBlogRetrieveUpdateDestroyView.as_view(), name='shelter-blog-detail'),
 ]
