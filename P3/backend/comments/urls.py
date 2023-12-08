e3ee04ac2d26b5d9c6c005584d7c1c15f6c1ca27
nomiko0626 <113321610+nomiko0626@users.noreply.github.com>
2023-12-08 16:06:52 -0500

    shelter blog comment backend

--- P3/backend/comments/urls.py
+++ P3/backend/comments/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ShelterCommentListCreateAPIView, ApplicationCommentListCreateAPIView
+from .views import ShelterCommentListCreateAPIView, ApplicationCommentListCreateAPIView, ShelterBlogCommentListCreateAPIView
 
 app_name='comments'
 urlpatterns = [
     path('shelter/<int:pk>/', ShelterCommentListCreateAPIView.as_view(), name='shelter-comment-list-create'),
     path('application/<int:pk>/', ApplicationCommentListCreateAPIView.as_view(), name='application-comment-list-create'),
+    path('shelter/<int:pk>/blog/', ShelterBlogCommentListCreateAPIView.as_view(), name='shelter-blog-comment-list-create'),
 ]
