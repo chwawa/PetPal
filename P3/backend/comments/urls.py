239d7a0944b777c95db2ce768e09ab1c1150466f
nomiko0626 <113321610+nomiko0626@users.noreply.github.com>
2023-12-09 13:43:00 -0500

    Update urls.py

--- P3/backend/comments/urls.py
+++ P3/backend/comments/urls.py
@@ -5,5 +5,5 @@ app_name='comments'
 urlpatterns = [
     path('shelter/<int:pk>/', ShelterCommentListCreateAPIView.as_view(), name='shelter-comment-list-create'),
     path('application/<int:pk>/', ApplicationCommentListCreateAPIView.as_view(), name='application-comment-list-create'),
-    path('shelter/<int:pk>/blog/', ShelterBlogCommentListCreateAPIView.as_view(), name='shelter-blog-comment-list-create'),
+    path('blog/<int:pk>/', ShelterBlogCommentListCreateAPIView.as_view(), name='shelter-blog-comment-list-create'),
 ]
