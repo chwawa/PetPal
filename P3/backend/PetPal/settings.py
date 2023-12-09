e9377741e970d75fac70e1a331e687d70a719a5d
Chelsea Wang <chelseawang9@gmail.com>
2023-12-09 02:15:18 -0500

    revert

--- P3/backend/PetPal/settings.py
+++ P3/backend/PetPal/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8&783r!-rp6*eo-4e9vuiyyqcjq)t-8!x#e^gi2wqz52qx&(x3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
+ALLOWED_HOSTS = []
 
 
 # Application definition
