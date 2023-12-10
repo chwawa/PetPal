a68b7b5cda9ea1af0de4e16010bbaa101049490e
Duan Liu <duan.liu@mail.utoronto.ca>
2023-12-09 20:14:03 -0500

    update frontend notification page

--- P3/frontend/src/components/NotificationCard/index.jsx
+++ P3/frontend/src/components/NotificationCard/index.jsx
@@ -4,23 +4,42 @@ import { Badge, Card, CloseButton } from "react-bootstrap";
 import "./NotificationCard.css"
 
 
-export default function NotificationCard( link, cardText, read, timeStamp) {
+export default function NotificationCard( {notifiID, link, message, read, timeStamp}) {
     let navigate = useNavigate();
+    const url = 'http://127.0.0.1:8000';
+    const accessToken = localStorage.getItem('access_token');
     
     const handleCardClick = () => {
         if (link) {
             navigate(link);
-            read = true;
         }
+        fetch(`${url}/notifications/${notifiID}`, {
+          method: 'GET',
+          headers: {
+            'Authorization': `Bearer ${accessToken}`,
+            'Content-Type': 'application/json',
+          },
+        })
+    }
+
+    async function deleteClick (notifiID) {
+        await fetch(`${url}/notifications/${notifiID}/`, {
+            method: 'DELETE',
+            headers: {
+            'Authorization': `Bearer ${accessToken}`,
+            'Content-Type': 'application/json',
+            },
+        })
+        window.location.reload();
     }
 
     return (
-      <Card onClick={() => handleCardClick()} style={{ cursor: "pointer"}}>
+      <Card className="card" style={{ cursor: "pointer"}} >
       <Card.Body>
-        {read ? null : <Badge pill bg="warning">New</Badge>}
-        <CloseButton />
-        <Card.Text>{cardText}</Card.Text>
-        <Card.Text>{timeStamp}</Card.Text>
+        {read ? null : <Badge className="unread" pill bg="warning" style={{fontSize:"1rem"}}>Unread</Badge>}
+        <CloseButton className="cross" onClick={() => deleteClick(notifiID)}/>
+        <Card.Text className="message" onClick={() => handleCardClick(notifiID)}>{message}</Card.Text>
+        <Card.Text className="time" onClick={() => handleCardClick(notifiID)}>{timeStamp}</Card.Text>
         </Card.Body>
       </Card>
     );
